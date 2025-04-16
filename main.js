49562bbd5b536ece9814cab438740e7a09bd6a8b
Your Name <dimasuws2009@gmail.com>
2025-04-16 11:33:01 +0700

    tugas kurang js

---
+++ main.js
@@ -0,0 +1,10 @@
+const tabs = document.querySelectorAll('.tab');
+const contents = document.getElementById('plans-content');
+
+const plans = {
+    basic: {
+        label: 'Basic',
+        title: 'Icon Sets',
+        price: '$60',
+    }
+}
